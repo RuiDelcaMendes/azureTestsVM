54ab6fa96e9cc8e9434cdc28a6bdd3c5e3c1d272
ruidelca <ruidelca@RDMWS2016DEV.up5o0nkt4vmejdyrb3aadi4dla.ax.internal.cloudapp.net>
2018-01-31 11:34:16 +0000

    1st Commit

---
+++ Car2/src/main/java/service/CarListServiceFactory.java
@@ -0,0 +1,38 @@
+package service;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.olingo.odata2.processor.api.jpa.ODataJPAContext;
+import org.apache.olingo.odata2.processor.api.jpa.ODataJPAServiceFactory;
+import org.apache.olingo.odata2.processor.api.jpa.exception.ODataJPARuntimeException;
+
+public class CarListServiceFactory extends ODataJPAServiceFactory {
+
+	private static final String PERSISTENCE_UNIT_NAME = "Car2";
+
+	@Override
+	public ODataJPAContext initializeODataJPAContext() throws ODataJPARuntimeException {
+
+		ODataJPAContext oDatJPAContext = this.getODataJPAContext();
+		
+
+		try {
+
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+
+			oDatJPAContext.setEntityManagerFactory(emf);
+
+			oDatJPAContext.setPersistenceUnitName(PERSISTENCE_UNIT_NAME);
+
+			return oDatJPAContext;
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			throw new RuntimeException(e);
+
+		}
+
+	}
+
+}
---
+++ Car2/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h1>Car List OData</h1><br><br>
+<a href="/Car2/CarListServlet.svc/">Service Document</a>
+</body>
+</html>
